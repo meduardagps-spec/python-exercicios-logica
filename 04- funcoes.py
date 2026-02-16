62a0cc89cbd79630d90b9004c126e4a828d0698f
mariaeduardasoftware <meduardagps@gmail.com>
2026-02-16 12:50:18 -0300

    Update and rename ex027.py to 04- funcoes.py

---
+++ 04- funcoes.py
@@ -0,0 +1,8 @@
+nome=input("Nome completo:").strip()
+
+nome=nome.split()
+
+print(f"O primeiro nome é {nome[0]}")
+
+print(f"O último nome é {nome[len(nome)-1]}")
+
--- ex027.py
+++
@@ -1,4 +0,0 @@
-nome=input("Nome completo:").strip()
-nome=nome.split()
-print(nome[0])
-print(nome[len(nome)-1])
