a9ab932bc36b1e967343f221a6e3b5f27613c08c
mariaeduardasoftware <meduardagps@gmail.com>
2026-02-16 12:38:24 -0300

    Fix variable name and formatting in ex019.py

--- ex019.py
+++ ex019.py
@@ -3,8 +3,9 @@ aluno_1 = str(input('Nome do primeiro aluno:'))
 aluno_2 = str(input('Nome do segundo aluno:'))
 aluno_3 = str(input('Nome do terceiro aluno:'))
 aLundo_4 = str(input('Nome do quarto aluno:'))
-Lista=[ aluno_1 , aluno_2 , aluno_3 , aluno_4 ]
+Lista = [ aluno_1 , aluno_2 , aluno_3 , aluno_4 ]
 escolhido = choice(Lista)
 print(f'A pessoa sorteada é {escolhido}!')
+
 
 
